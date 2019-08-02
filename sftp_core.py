3cca432a63781d924af0e4935fd66aa78de176d9
jhon.serrano <jserrano2610@gmail.com>
2019-08-02 09:00:33 -0400

    Rename of variables.

--- sftp_core.py
+++ sftp_core.py
@@ -19,42 +19,42 @@ cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None  
 
 # Credenciales para autenticar con los dispositivos;  indice de tupla : [0 = id, 1 = username, 2 = password encrypted]
-x = select_tables_by_row(conn, "credential", 2)
+credentials = select_tables_by_row(conn, "credential", 2)
 
 # Descifrador de password!
-pass_clear = enpass(x[2])
+pass_clear = enpass(credentials[2])
 
 # Datos de los dispositivos; indice de tupla : [0 = id, 1 = Hostname, 2 = IP o nombre DNS, 3 = Tipo de Dispositivo]
-y = select_all_in_table(conn, "device_data")
+device_data = select_all_in_table(conn, "device_data")
 
 # Bucle "for in" para iterar el respaldo de todo los dispositivos habidos en la tabla "device_data"
 # También se agrega las credenciales, pero debido a que los dispositivos están contra un AAA, tienen la misma contraseña.
-for it in y:
+for data in device_data:
     # Localización de los directorios donde se resguardará el respaldo.
     # Entre corchetes está la variable de acuerdo al dispositivo.
     # Ruta local de directorio = localDirPath; Ruta local de archivos = localFilePath
-    localDirPath = f'./DATA/{ it[1] }'
+    localDirPath = f'./DATA/{ data[1] }'
     localFilePath = f'{ localDirPath }/{ datetime.now().strftime("%Y-%m-%d") }.txt'
     # Condicional if - else comprueba si existe el archivo de respaldo, de lo contrario finaliza la sentencia.
     if os.path.exists(localFilePath):
         # Variable wlog() escribe lo indicado en un archivo log
-        wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Archivo de respaldo { it[1] } ya existe!")
+        wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Archivo de respaldo { data[1] } ya existe!")
     else:
         # Sentencias "try except else" manejan errores si se llegacen a presentar
         try:
-            sftp = pysftp.Connection(host=it[2], username=x[1], password=pass_clear, cnopts=cnopts)
+            sftp = pysftp.Connection(host=data[2], username=credentials[1], password=pass_clear, cnopts=cnopts)
         except KeyboardInterrupt:
-            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Script detenido inesperadamente por el usuario")
-            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Tiempo de ejecución del script: { round( (time.time() - start_time), 2) } segundos.")
+            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Script detenido inesperadamente por el usuario")
+            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Tiempo de ejecución del script: { round( (time.time() - start_time), 2) } segundos.")
             quit()
         except:
-            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   No es posible la conexión con { it[1] }: ¡tiempo agotado!")
+            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   No es posible la conexión con { data[1] }: ¡tiempo agotado!")
         else:
-            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Intentando conectar { it[1] } ... ")
+            wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Intentando conectar { data[1] } ... ")
 
             # Define a que directorio remoto del equipo de red debes elegir para descargar
             # En el archivo devices_def.py se definen un diccionario que indica el tipo y el path.
-            remoteFilePath = switch(it[3])
+            remoteFilePath = switch(data[3])
 
         # Define the local path where the file will be saved
         # or absolute "C:\Users\sdkca\Desktop\TUTORIAL.txt"
@@ -65,16 +65,16 @@ for it in y:
             # Captura el error en caso de ocurrir alguno, y escribe los mensajes en el log.
             try:
                 sftp.get(remoteFilePath, localFilePath)
-                wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Respaldo del archivo conf en el dispositivo { it[1] } realizado exitosamente!")
+                wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Respaldo del archivo conf en el dispositivo { data[1] } realizado exitosamente!")
             except:
-                wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Error en la conexión con el dispositivo { it[1] }. ¿Está habilitado SFTP en este equipo?")
+                wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Error en la conexión con el dispositivo { data[1] }. ¿Está habilitado SFTP en este equipo?")
             sftp.close()
 
 
 # Variable para el cálculo de la duración total de la ejecución del script.
 # El resultado es almacenado en el log.
 rtime = (time.time() - start_time)
-wlog(f"{ datetime.now().strftime('%Y-%m-%d %H-%M-%S') }   Tiempo de ejecución del script: { round(rtime, 2) } segundos.")
+wlog(f"{ datetime.now().strftime('%Y-%m-%d %H:%M:%S') }   Tiempo de ejecución del script: { round(rtime, 2) } segundos.")
 
 # # connection closed automatically at the end of the with-block
 
