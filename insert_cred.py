8b3b5e8b41cc0c142510d677bdba60f68fcf7f42
jhon.serrano <jserrano2610@gmail.com>
2019-08-24 20:04:07 -0400

    Elementary changes!

--- insert_cred.py
+++ insert_cred.py
@@ -1,12 +1,22 @@
 import sqlite3
 from encryptor import passwrd
+import os
+import getpass
+import write_log
+from write_log import log_error, log_warning, log_info, log_debug
+
+db_dir = os.path.abspath(os.getcwd()) + "/database"
+print(db_dir)
+if not os.path.exists(db_dir):
+    os.makedirs(db_dir)
+    log_info("Se creó directorio para guardar la base de datos!")
 
 
 # Introducción y validación de datos entrantes
 print("*** Script para insertar las credenciales de los equipos ***")
 
 username = input("Introduzca el nombre del usuario: ")
-password = input("Introduzca la contraseña: ")
+password = getpass.getpass("Introduzca la contraseña: ")
 
 try: username = str(username)
 except ValueError:
@@ -21,7 +31,7 @@ except ValueError:
 passenc = passwrd(password)
 
 # Conectar la base de datos
-conection = sqlite3.connect("database/schema.db")
+conection = sqlite3.connect(db_dir + "/schema.db")
 
 # Seleccionar el cursor para realizar la consulta
 c = conection.cursor()
@@ -29,6 +39,7 @@ c = conection.cursor()
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='credential' ''')
 
 if c.fetchone()[0]==0 :
+    log_info("Creando esquemas para la tabla <credencial> ... ")
 
     c.execute(
     """
@@ -50,10 +61,15 @@ VALUES (?, ?);
 
 # Realizar la consulta
 # Ejecutar la consulta
-if (c.execute(sql, arguments)):
-    print("¡Registros guardados exitosamente!")
-else:
-    print("¡Ocurrió un error con la inserción de datos!")
+try :
+    if (c.execute(sql, arguments)):
+        print("¡Registros guardados exitosamente!")
+    else:
+        print("¡Ocurrió un error con la inserción de datos!")
+
+except sqlite3.IntegrityError :
+    print("Inserción de credenciales inválida: los usuarios debe ser únicos en la base de datos!")
+    log_warning("Inserción de credenciales inválida: los usuarios debe ser únicos en la base de datos!")
 
 # Terminamos la consulta
 c.close()
